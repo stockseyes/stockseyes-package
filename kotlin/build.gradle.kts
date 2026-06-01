046677a5733f4466ec3a80d993cf7822e5c16d06
Srijan Trivedi <Srijantrivedi445@gmail.com>
2026-06-01 17:40:40 +0530

    Pkg for kotlin

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -71,4 +71,15 @@ publishing {
             }
         }
     }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = System.getenv("MAVEN_USERNAME") ?: ""
+                password = System.getenv("MAVEN_PASSWORD") ?: ""
+            }
+        }
+    }
 }
